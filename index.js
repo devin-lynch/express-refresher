8e31a0d75c128584242d3b96e5580310efbbe02c
devin-lynch <devin.l.lynch@gmail.com>
2022-12-06 17:58:00 -0600

    Add ejs routes

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express()
+const ejsLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('home');
 });
 
-app.listen(8000)
+app.get('/animals', (req, res) => {
+    res.render('animals', {animals: ['sand crab', 'corny joke dog']})
+});
+
+app.get('/foods', (req, res) => {
+    res.render('foods', {foods: ['fajitas', 'ramen']})
+});
+
+app.get('/movies', (req, res) => {
+    res.render('movies', {movies: ['perfect blue', 'one piece: film red']})
+});
+
+app.get('/products', (req, res) => {
+    res.render('products', {products: ['keebs', 'desk fan']})
+});
+
+
+
+app.listen(3000)
